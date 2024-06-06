394ccc21ff1fc9fa21700f4ce9866c2737861f9f
Nebula580 <evandiplacido@outlook.com>
2024-06-06 02:11:32 -0600

    Fixes

--- Lab 0/obj/Debug/net8.0/Lab 0.AssemblyInfo.cs
+++ Lab 0/obj/Debug/net8.0/Lab 0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab 0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115731e3303e1dc019cd8c1dabba7f44da89e391")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab 0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab 0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
